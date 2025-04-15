ba53aa49fcfe76c201d187368c51433292cc9de0
MartinVilladsen <martin.k.villadsen@gmail.com>
2025-04-15 13:36:37 +0200

    Initial Commit

---
+++ Birdspotting/BirdspottingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdspottingApp.swift
+//  Birdspotting
+//
+//  Created by Natasja Knak Villadsen on 15/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BirdspottingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
